1f75f41588a230c4b822eb7d97dc7cf9801a9678
nelson_html <martineznelson515@gmail.com>
2024-04-24 16:39:40 -0600

    subiendo todos los archivos de c

---
+++ declaracion_variable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	//Declaramos una variable
+	int miNumero;
+	
+	//la inicializamos
+	miNumero = 10;
+	
+	//imprimir la varaible
+	//formato %d -> decimal
+	printf("Mi numero es: %d", miNumero);
+	
+	//1. declaramos e inicializamos la variable
+	int miNumero2 = 20, miNumero3 = 30;
+	
+	//2. Imprimir el valor de las variables
+	printf ("\nMi numero dos es: %d, Mi numero tres es: %d", miNumero2, miNumero3);
+	
+	return 0;
+}
+
---
+++ imprimir_hola_mundo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/*Programa:
+Imprimir hola mundo
+*/
+
+int main(){
+	//mandamos a imprimir en la consola
+	printf("Hola mundo desde C...");
+	return 0;
+	
+	
+}
